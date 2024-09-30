c81be52eebb6d55a71a6478ffd91b35a27b921d0
Diego Rodriguez <alejandro27062@gmail.com>
2024-09-30 15:07:30 -0500

    fix: protecting auth route

--- src/app/api/get-token/route.ts
+++ src/app/api/get-token/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 
 import { auth } from "@/auth";
 
-export async function GET() {
-  const session = await auth();
+export const GET = auth(function GET(req) {
+  if (req.auth) return NextResponse.json({ token: req.auth.user.token });
 
-  return NextResponse.json({ token: session?.user.token });
-}
+  return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
+});
