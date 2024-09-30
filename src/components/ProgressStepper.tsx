f02b57409570af2a2250984dc69f2993db417378
Diego Rodriguez <alejandro27062@gmail.com>
2024-09-30 14:01:37 -0500

    feat: steps as buttons

--- src/components/ProgressStepper.tsx
+++ src/components/ProgressStepper.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@nextui-org/button";
 import FeatherIcon from "feather-icons-react";
 
 type Props = {
@@ -9,20 +10,36 @@ type Props = {
 
 function ProgressStepper({ index, completed, title, description }: Props) {
   return (
-    <div className="flex items-center gap-4">
-      <div
-        className={`${completed ? "bg-secondary text-black" : "text-secondary"} rounded-full min-w-[40px] min-h-[40px] grid place-items-center border-1 border-secondary stepper-line-content`}
-      >
-        {index}
+    <Button
+      className="border-0 p-3 bg-transparent flex justify-start whitespace-normal h-auto"
+      endContent={
+        completed ? (
+          <FeatherIcon
+            className="text-secondary-500"
+            icon="check-circle"
+            size={20}
+          />
+        ) : (
+          <FeatherIcon
+            className="text-secondary-500"
+            icon="external-link"
+            size={20}
+          />
+        )
+      }
+    >
+      <div className="flex items-center w-full gap-4">
+        <div
+          className={`${completed ? "bg-secondary text-black" : "text-secondary"} rounded-full min-w-[40px] min-h-[40px] grid place-items-center border-1 border-secondary stepper-line-content`}
+        >
+          {index}
+        </div>
+        <div className="text-sm text-start">
+          <p className="font-medium">{title}</p>
+          <p className="text-gray-500">{description}</p>
+        </div>
       </div>
-      <div className="text-sm">
-        <p className="font-medium">{title}</p>
-        <p className="text-gray-500">{description}</p>
-      </div>
-      {completed && (
-        <FeatherIcon className="text-secondary-500" icon="check-circle" />
-      )}
-    </div>
+    </Button>
   );
 }
 
