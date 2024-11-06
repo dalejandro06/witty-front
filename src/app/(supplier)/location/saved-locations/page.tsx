2379b40c12d40469b7420df450dcbeca45126feb
Diego Rodriguez <alejandro27062@gmail.com>
2024-11-06 15:48:07 -0500

    feat: add location button to end

--- src/app/(supplier)/location/saved-locations/page.tsx
+++ src/app/(supplier)/location/saved-locations/page.tsx
@@ -1,7 +1,6 @@
-import { Button, Link } from "@nextui-org/react";
-import Image from "next/image";
+import { Button } from "@nextui-org/button";
+import { Link } from "@nextui-org/link";
 
-import NoLocation from "@/assets/no-locations.png";
 import { getSupplierLocations } from "@/repositories/ApiRepository";
 import LocationsList from "@/app/modules/supplier-location/LocationsList";
 import TitleInfo from "@/components/TitleInfo";
@@ -10,37 +9,25 @@ async function SavedLocations() {
   const locations = await getSupplierLocations();
 
   return (
-    <div className="my-5 relative h-full">
-      <TitleInfo
-        infoText="Busca y filtra tus ubicaciones en donde ofreces tus servicios."
-        title="Tus ubicaciones"
-      />
-      {locations.length > 0 ? (
+    <section className="flex flex-col justify-between h-full">
+      <div>
+        <TitleInfo
+          infoText="Busca y filtra tus ubicaciones en donde ofreces tus servicios."
+          title="Tus ubicaciones"
+        />
         <LocationsList locations={locations} />
-      ) : (
-        <div className="grid place-items-center gap-6 mt-20">
-          <Image
-            alt="Not locations banner"
-            height={150}
-            src={NoLocation}
-            width={150}
-          />
-          <div className="text-center">
-            <p>Aún no has generado ubicaciones.</p>
-            <p>¡Agrega una para empezar!</p>
-          </div>
-          <Button
-            showAnchorIcon
-            as={Link}
-            className="bg-gray-100 text-black"
-            href="/location/add-location"
-            size="lg"
-          >
-            Crea una ubicación
-          </Button>
-        </div>
-      )}
-    </div>
+      </div>
+      <Button
+        fullWidth
+        as={Link}
+        className="text-black py-5 mt-10"
+        color="secondary"
+        href="/location/add-location"
+        size="lg"
+      >
+        Añade una ubicación
+      </Button>
+    </section>
   );
 }
 
--- src/app/modules/supplier-location/LocationsList.tsx
+++ src/app/modules/supplier-location/LocationsList.tsx
@@ -1,39 +1,54 @@
 "use client";
 
 import { Button } from "@nextui-org/button";
-import { useRouter } from "next/navigation";
+import { Link } from "@nextui-org/link";
+import Image from "next/image";
 
 import LocationCard from "@/components/LocationCard";
 import { SupplierLocation } from "@/types/ApiTypes";
+import NoLocation from "@/assets/no-locations.png";
 
 type Props = {
   locations: SupplierLocation[];
 };
 function LocationsList({ locations }: Props) {
-  const router = useRouter();
+  if (!locations.length) {
+    return (
+      <div className="grid place-items-center gap-6 mt-20">
+        <Image
+          alt="Not locations banner"
+          height={150}
+          src={NoLocation}
+          width={150}
+        />
+        <div className="text-center">
+          <p>Aún no has generado ubicaciones.</p>
+          <p>¡Agrega una para empezar!</p>
+        </div>
+        <Button
+          showAnchorIcon
+          as={Link}
+          className="bg-gray-100 text-black"
+          href="/location/add-location"
+          size="lg"
+        >
+          Crea una ubicación
+        </Button>
+      </div>
+    );
+  }
 
   return (
-    <>
-      <div className="mt-10 grid gap-5">
-        {locations.map((location) => (
-          <LocationCard
-            key={location.id}
-            isDisabled={!location.status}
-            isPrincipal={location.available_all_services}
-            location={location}
-          />
-        ))}
-      </div>
-      <Button
-        fullWidth
-        className="text-black py-5 mt-10"
-        color="secondary"
-        size="lg"
-        onPress={() => router.push("/location/add-location")}
-      >
-        Añade una ubicación
-      </Button>
-    </>
+    <div className="mt-10 grid gap-5">
+      {locations.map((location) => (
+        <LocationCard
+          key={location.id}
+          isDisabled={!location.status}
+          isPrincipal={location.available_all_services}
+          location={location}
+        />
+      ))}
+    </div>
   );
 }
 
