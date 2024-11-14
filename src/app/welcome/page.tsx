922c06a01e4016c23d0b3f94ca01dbc435923384
Diego Rodriguez <alejandro27062@gmail.com>
2024-11-13 20:57:14 -0500

    fix: change welcome page and redirect based on user role

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 import { welcomeWords } from "@/app/modules/welcome/welcomeData";
 import ColorText from "@/app/modules/welcome/ColorText";
 import Logo from "@/assets/Logo.svg";
 import FooterButton from "@/app/modules/welcome/FooterButton";
+import { auth } from "@/auth";
 
-export default function welcome() {
+export default async function welcome() {
+  const session = await auth();
+
+  if (session) {
+    const redirectTo = session.user.supplier?.id ? "/supplier" : "/client";
+
+    redirect(redirectTo);
+  }
+
   return (
     <section className="bg-blue-900 flex flex-col px-6 pt-16 gap-4 w-100 relative h-screen">
       <div className="flex justify-center">
